c90d2a866d10427e101dde2e337677a26b3e71b5
Marius Gafton <mariusgafton2005@gmail.com>
2025-04-27 11:10:56 +0200

    GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteCompression from "vite-plugin-compression";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteCompression({
-      algorithm: "gzip", 
-      ext: ".gz", 
-    }),
-  ],
+  base: '/PokeDex/',  
+  plugins: [react()],
 });
